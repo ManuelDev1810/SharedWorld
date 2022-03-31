22519f26122c8ac7bbf7aa2a09106bf74a6d3ae0
jcordero-finitive <m.guerrero1@unapec.edu.do>
2022-03-30 21:48:08 -0400

    adding users layout

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,22 @@
+import User from "./User";
+
+const Users: React.FC = () => {
+    return (
+        <>
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+            <User />
+        </>
+    );
+};
+
+export default Users;
